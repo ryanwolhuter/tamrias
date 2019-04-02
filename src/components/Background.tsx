ad1d9a23a128662adca88fef7bc65163bb20e8dc
ry <dev@ryanwolhuter.com>
2019-04-02 13:01:09 +0200

    Add shaded background to text divs

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -12,7 +12,7 @@ export function Background() {
   useEffect(() => {
     document.addEventListener('resize', updateWidthAndHeight);
     return document.removeEventListener('resize', updateWidthAndHeight);
-  }, [width, height]);
+  }, []);
 
   function updateWidthAndHeight() {
     setWidth(document.documentElement.clientWidth);
@@ -24,6 +24,51 @@ export function Background() {
       width={`${width}px`}
       height={`${height}px`}
       className='background'
+      params={{
+        particles: {
+          number: {
+            value: 80,
+            density: {
+              enable: true,
+              value_area: 800
+            }
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 40,
+              size_min: 0.1,
+              sync: false
+            }
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: 'top',
+            random: true,
+            straight: true,
+            out_mode: 'out',
+            bounce: false,
+            attract: {
+              enable: false,
+            }
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: false,
+              },
+              onclick: {
+                enable: false,
+              },
+              resize: true
+            },
+          },
+          retina_detect: true
+        }
+      }}
     />
   );
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,12 +5,14 @@ export function Contact() {
   return (
     <section id="contact">
       <div className="contact-container">
-        <h1>Contact</h1>
+        <div className="text">
+          <h1>Contact</h1>
 
-        <p>If you're not down to jam don't even 
+          <p>If you're not down to jam don't even
           talk to me.</p>
 
-        <p>No stalkers. No weirdos. No Furries.</p>
+          <p>No stalkers. No weirdos. No Furries.</p>
+        </div>
       </div>
     </section>
   );
